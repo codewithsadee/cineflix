8b0ef62f81bd23ef5cd0bd664c8205ea9cdc3796
Sadee <mohammadsadee24@gmail.com>
2022-06-01 17:26:35 +0600

    initial

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// variables for navbar menu toggle
+const header = document.querySelector('header');
+const nav = document.querySelector('nav');
+const navbarMenuBtn = document.querySelector('.navbar-menu-btn');
+
+// variables for navbar search toggle
+const navbarForm = document.querySelector('.navbar-form');
+const navbarFormCloseBtn = document.querySelector('.navbar-form-close');
+const navbarSearchBtn = document.querySelector('.navbar-search-btn');
+
+
+// navbar menu toggle function
+function navIsActive() {
+  header.classList.toggle('active');
+  nav.classList.toggle('active');
+  navbarMenuBtn.classList.toggle('active');
+}
+
+navbarMenuBtn.addEventListener('click', navIsActive);
+
+
+
+// navbar search toggle function
+const searchBarIsActive = () => navbarForm.classList.toggle('active');
+
+navbarSearchBtn.addEventListener('click', searchBarIsActive);
+navbarFormCloseBtn.addEventListener('click', searchBarIsActive);
